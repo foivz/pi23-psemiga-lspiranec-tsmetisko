ad02421b22930b2d6ce630d28923da2fa7dfa563
Leon Špiranec <lspiranec@foi.hr>
2023-06-08 16:01:35 +0200

    Implementacija forme za registraciju

---
+++ Software/Projekt_Rent-A-Field/Projekt_Rent-A-Field/Registracija.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Projekt_Rent_A_Field
+{
+    partial class Registracija
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKorIme = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPonovljenaLozinka = new System.Windows.Forms.TextBox();
+            this.txtLozinka = new System.Windows.Forms.TextBox();
+            this.txtBrojMobitela = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtImePrezime = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnRegistriraj = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtKorIme
+            // 
+            this.txtKorIme.Location = new System.Drawing.Point(178, 225);
+            this.txtKorIme.Name = "txtKorIme";
+            this.txtKorIme.Size = new System.Drawing.Size(186, 22);
+            this.txtKorIme.TabIndex = 44;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 231);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 16);
+            this.label7.TabIndex = 43;
+            this.label7.Text = "Korisnicko ime:";
+            // 
+            // txtPonovljenaLozinka
+            // 
+            this.txtPonovljenaLozinka.Location = new System.Drawing.Point(178, 299);
+            this.txtPonovljenaLozinka.Name = "txtPonovljenaLozinka";
+            this.txtPonovljenaLozinka.Size = new System.Drawing.Size(186, 22);
+            this.txtPonovljenaLozinka.TabIndex = 42;
+            // 
+            // txtLozinka
+            // 
+            this.txtLozinka.Location = new System.Drawing.Point(178, 262);
+            this.txtLozinka.Name = "txtLozinka";
+            this.txtLozinka.Size = new System.Drawing.Size(186, 22);
+            this.txtLozinka.TabIndex = 41;
+            // 
+            // txtBrojMobitela
+            // 
+            this.txtBrojMobitela.Location = new System.Drawing.Point(178, 189);
+            this.txtBrojMobitela.Name = "txtBrojMobitela";
+            this.txtBrojMobitela.Size = new System.Drawing.Size(186, 22);
+            this.txtBrojMobitela.TabIndex = 40;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(178, 147);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(186, 22);
+            this.txtEmail.TabIndex = 39;
+            // 
+            // txtImePrezime
+            // 
+            this.txtImePrezime.Location = new System.Drawing.Point(178, 107);
+            this.txtImePrezime.Name = "txtImePrezime";
+            this.txtImePrezime.Size = new System.Drawing.Size(186, 22);
+            this.txtImePrezime.TabIndex = 38;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(218, 339);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(146, 46);
+            this.btnClose.TabIndex = 37;
+            this.btnClose.Text = "Odustani";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnRegistriraj
+            // 
+            this.btnRegistriraj.Location = new System.Drawing.Point(26, 339);
+            this.btnRegistriraj.Name = "btnRegistriraj";
+            this.btnRegistriraj.Size = new System.Drawing.Size(146, 46);
+            this.btnRegistriraj.TabIndex = 36;
+            this.btnRegistriraj.Text = "Registriraj se";
+            this.btnRegistriraj.UseVisualStyleBackColor = true;
+            this.btnRegistriraj.Click += new System.EventHandler(this.btnRegistriraj_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(97, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(176, 32);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "Registracija";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 302);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 16);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "Ponovite lozinku:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 265);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 16);
+            this.label4.TabIndex = 33;
+            this.label4.Text = "Lozinka:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 16);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "Broj mobitela: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Email:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 16);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Ime i prezime:";
+            // 
+            // Registracija
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 427);
+            this.Controls.Add(this.txtKorIme);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtPonovljenaLozinka);
+            this.Controls.Add(this.txtLozinka);
+            this.Controls.Add(this.txtBrojMobitela);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtImePrezime);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnRegistriraj);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Registracija";
+            this.Text = "Registracija";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKorIme;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtPonovljenaLozinka;
+        private System.Windows.Forms.TextBox txtLozinka;
+        private System.Windows.Forms.TextBox txtBrojMobitela;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtImePrezime;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnRegistriraj;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Software/Projekt_Rent-A-Field/Projekt_Rent-A-Field/Registracija.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Validation;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt_Rent_A_Field
+{
+    public partial class Registracija : Form
+    {
+        public Registracija()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private int ProvjeriUnose(string imePrezime, string email, string brojMobitela, string korIme, string lozinka)
+        {
+            if (string.IsNullOrEmpty(imePrezime) || string.IsNullOrWhiteSpace(imePrezime) || string.IsNullOrEmpty(email) || string.IsNullOrWhiteSpace(email) ||
+                string.IsNullOrEmpty(brojMobitela) || string.IsNullOrWhiteSpace(brojMobitela) || string.IsNullOrEmpty(korIme) || string.IsNullOrWhiteSpace(korIme) ||
+                string.IsNullOrEmpty(lozinka) || string.IsNullOrWhiteSpace(lozinka))
+            {
+                return -1;
+            }
+            else if (!ProvjeraImenaPrezimena(imePrezime))
+            {
+                return -2;
+            }
+            else if (!ProvjeraMobitela(brojMobitela))
+            {
+                return -3;
+            }
+            else if (!ProvjeraEmaila(email))
+            {
+                return -4;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        private bool ProvjeraImenaPrezimena(string unos)
+        {
+            string uzorak = @"^(([A-Z,ČĆŽĐŠ][a-z,čćžđš]{1,})(([ ]|[-])([A-Z,ČĆŽĐŠ][a-z,čćžđš]{1,}))?)$";
+            if (!Regex.Match(unos, uzorak).Success) return false; else return true;
+
+        }
+
+        private bool ProvjeraMobitela(string unos)
+        {
+            string uzorak = @"^[0-9]{3}-[0-9]{3}-[0-9]{4}$";
+            if (!Regex.Match(unos, uzorak).Success) return false; else return true;
+        }
+
+        private bool ProvjeraEmaila(string unos)
+        {
+            string uzorak = @"^([A-Z,a-z,ČĆŽĐŠčćžđš][a-z,A-Z,0-9,ČĆŽĐŠčćžđš,_,-,.]{2,}@[a-z]{2,}[.][a-z][a-z]{1,})$";
+            if (!Regex.Match(unos, uzorak).Success) return false; else return true;
+        }
+
+        public int Genereriraj()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 1000);
+            return randomNumber;
+        }
+
+        private void btnRegistriraj_Click(object sender, EventArgs e)
+        {
+            using (var context = new PI2306_DBEntities())
+            {
+                int korisnik_id = Genereriraj();
+                string imePrezime = txtImePrezime.Text;
+                string email = txtEmail.Text;
+                string brojMobitela = txtBrojMobitela.Text;
+                string korIme = txtKorIme.Text;
+                string lozinka = txtLozinka.Text;
+                Korisnik korisnik = new Korisnik()
+                {
+                    korisnik_id = korisnik_id,
+                    ime_prezime = imePrezime,
+                    email = email,
+                    broj_mobitela = brojMobitela,
+                    korisnicko_ime = korIme,
+                    lozinka = lozinka,
+                    uloga_id = 2
+                };
+                int uspjesno = ProvjeriUnose(imePrezime, email, brojMobitela, korIme, lozinka);
+                string poruka = "";
+                switch (uspjesno)
+                {
+                    case -1:
+                        poruka = "Niste popunili sva polja!";
+                        break;
+                    case -2:
+                        poruka = "Ime i prezime su pogrešnog formata!";
+                        break;
+                    case -3:
+                        poruka = "Broj mobitela je pogrešnog formata!";
+                        break;
+                    case -4:
+                        poruka = "Email je pogrešnog formata!";
+                        break;
+                    case 1:
+                        try
+                        {
+                            context.Korisniks.Add(korisnik);
+                            context.SaveChanges();
+                        }
+                        catch (DbEntityValidationException ex)
+                        {
+                            foreach (var eve in ex.EntityValidationErrors)
+                            {
+                                Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                                    eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                                foreach (var ve in eve.ValidationErrors)
+                                {
+                                    Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                                        ve.PropertyName, ve.ErrorMessage);
+                                }
+                            }
+                            throw;
+                        }
+
+                        context.SaveChanges();
+                        //GeneriranjeQR generiranje = new GeneriranjeQR(email, lozinka);
+                        //generiranje.Show();
+
+                        break;
+                }
+                if (uspjesno != 1)
+                {
+                    MessageBox.Show(poruka);
+                }
+            }
+        }
+    }
+}
