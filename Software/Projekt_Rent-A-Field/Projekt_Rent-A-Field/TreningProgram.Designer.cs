afe128410cdcfe03213acea26ae917cec5f8a9a8
Leon Špiranec <lspiranec@foi.hr>
2023-06-09 13:09:15 +0200

    Implementacija forme za izradu vlastitog trening programa

---
+++ Software/Projekt_Rent-A-Field/Projekt_Rent-A-Field/TreningProgram.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Projekt_Rent_A_Field
+{
+    partial class TreningProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbTip = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtVlasnik = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOpis = new System.Windows.Forms.RichTextBox();
+            this.txtNaziv = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbTip
+            // 
+            this.cmbTip.FormattingEnabled = true;
+            this.cmbTip.Location = new System.Drawing.Point(168, 80);
+            this.cmbTip.Name = "cmbTip";
+            this.cmbTip.Size = new System.Drawing.Size(351, 24);
+            this.cmbTip.TabIndex = 21;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 16);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "TIp programa:";
+            // 
+            // txtVlasnik
+            // 
+            this.txtVlasnik.Location = new System.Drawing.Point(168, 306);
+            this.txtVlasnik.Name = "txtVlasnik";
+            this.txtVlasnik.Size = new System.Drawing.Size(351, 22);
+            this.txtVlasnik.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 306);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 16);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Vlasnik programa:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(366, 343);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 44);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Odustani";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(168, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 44);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Izradi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Opis programa:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Naziv programa:";
+            // 
+            // txtOpis
+            // 
+            this.txtOpis.Location = new System.Drawing.Point(168, 159);
+            this.txtOpis.Name = "txtOpis";
+            this.txtOpis.Size = new System.Drawing.Size(351, 132);
+            this.txtOpis.TabIndex = 13;
+            this.txtOpis.Text = "";
+            // 
+            // txtNaziv
+            // 
+            this.txtNaziv.Location = new System.Drawing.Point(168, 119);
+            this.txtNaziv.Name = "txtNaziv";
+            this.txtNaziv.Size = new System.Drawing.Size(351, 22);
+            this.txtNaziv.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(409, 32);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Izradi vlastiti trening program";
+            // 
+            // TreningProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 406);
+            this.Controls.Add(this.cmbTip);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtVlasnik);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOpis);
+            this.Controls.Add(this.txtNaziv);
+            this.Controls.Add(this.label1);
+            this.Name = "TreningProgram";
+            this.Text = "TreningProgram";
+            this.Load += new System.EventHandler(this.TreningProgram_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbTip;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtVlasnik;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txtOpis;
+        private System.Windows.Forms.TextBox txtNaziv;
+        private System.Windows.Forms.Label label1;
+    }
+}
