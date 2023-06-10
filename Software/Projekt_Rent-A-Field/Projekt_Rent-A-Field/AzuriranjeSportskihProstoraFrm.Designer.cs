737cc366dfeb10db969d8e8c2814934f155bc73c
Petar Šemiga <psemiga@foi.hr>
2023-06-10 18:19:56 +0200

    Forma za azuriranje Sportskih prostora

---
+++ Software/Projekt_Rent-A-Field/Projekt_Rent-A-Field/AzuriranjeSportskihProstoraFrm.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Projekt_Rent_A_Field
+{
+    partial class AzuriranjeSportskihProstoraFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCijena = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAdresa = new System.Windows.Forms.TextBox();
+            this.txtNaziv = new System.Windows.Forms.TextBox();
+            this.btnOdustani = new System.Windows.Forms.Button();
+            this.btnAzuriraj = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCijena
+            // 
+            this.txtCijena.Location = new System.Drawing.Point(12, 106);
+            this.txtCijena.Name = "txtCijena";
+            this.txtCijena.Size = new System.Drawing.Size(216, 20);
+            this.txtCijena.TabIndex = 24;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(9, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Cijena:";
+            // 
+            // txtAdresa
+            // 
+            this.txtAdresa.Location = new System.Drawing.Point(12, 66);
+            this.txtAdresa.Name = "txtAdresa";
+            this.txtAdresa.Size = new System.Drawing.Size(216, 20);
+            this.txtAdresa.TabIndex = 22;
+            // 
+            // txtNaziv
+            // 
+            this.txtNaziv.Location = new System.Drawing.Point(12, 27);
+            this.txtNaziv.Name = "txtNaziv";
+            this.txtNaziv.Size = new System.Drawing.Size(216, 20);
+            this.txtNaziv.TabIndex = 21;
+            // 
+            // btnOdustani
+            // 
+            this.btnOdustani.Location = new System.Drawing.Point(121, 141);
+            this.btnOdustani.Name = "btnOdustani";
+            this.btnOdustani.Size = new System.Drawing.Size(107, 34);
+            this.btnOdustani.TabIndex = 20;
+            this.btnOdustani.Text = "Odustani";
+            this.btnOdustani.UseVisualStyleBackColor = true;
+            // 
+            // btnAzuriraj
+            // 
+            this.btnAzuriraj.Location = new System.Drawing.Point(12, 141);
+            this.btnAzuriraj.Name = "btnAzuriraj";
+            this.btnAzuriraj.Size = new System.Drawing.Size(107, 34);
+            this.btnAzuriraj.TabIndex = 19;
+            this.btnAzuriraj.Text = "Azuriraj";
+            this.btnAzuriraj.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(9, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Adresa:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(9, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Naziv:";
+            // 
+            // AzuriranjeSportskihProstoraFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(246, 193);
+            this.Controls.Add(this.txtCijena);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAdresa);
+            this.Controls.Add(this.txtNaziv);
+            this.Controls.Add(this.btnOdustani);
+            this.Controls.Add(this.btnAzuriraj);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AzuriranjeSportskihProstoraFrm";
+            this.Text = "AzuriranjeSportskihProstoraFrm";
+            this.Load += new System.EventHandler(this.AzuriranjeSportskihProstoraFrm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCijena;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAdresa;
+        private System.Windows.Forms.TextBox txtNaziv;
+        private System.Windows.Forms.Button btnOdustani;
+        private System.Windows.Forms.Button btnAzuriraj;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Software/Projekt_Rent-A-Field/Projekt_Rent-A-Field/AzuriranjeSportskihProstoraFrm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt_Rent_A_Field
+{
+    public partial class AzuriranjeSportskihProstoraFrm : Form
+    {
+        private Sportski_prostor odabrani;
+        public AzuriranjeSportskihProstoraFrm(Sportski_prostor sportski_prostor)
+        {
+            InitializeComponent();
+            odabrani = sportski_prostor;
+        }
+
+        private void AzuriranjeSportskihProstoraFrm_Load(object sender, EventArgs e)
+        {
+            txtNaziv.Text = odabrani.naziv;
+            txtAdresa.Text = odabrani.adresa;
+            txtCijena.Text = odabrani.cijena.ToString();
+        }
+
+        private void btnAzuriraj_Click(object sender, EventArgs e)
+        {
+            using (var context = new PI2306_DBEntities())
+            {
+                string naziv = txtNaziv.Text;
+                string adresa = txtAdresa.Text;
+                float cijena = float.Parse(txtCijena.Text);
+
+                context.Sportski_prostor.Attach(odabrani);
+
+                odabrani.naziv = naziv;
+                odabrani.adresa = adresa;
+                odabrani.cijena = cijena;
+
+                context.SaveChanges();
+            }
+            Close();
+            MessageBox.Show("Uspjesno azuriranje!");
+        }
+    }
+}
