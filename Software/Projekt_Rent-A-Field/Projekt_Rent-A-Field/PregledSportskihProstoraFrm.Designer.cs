7b071065ded4c226a6da621000b77c524fe849b0
Petar Šemiga <psemiga@foi.hr>
2023-06-10 18:08:34 +0200

    Forma za pregled Sportskih prostora

---
+++ Software/Projekt_Rent-A-Field/Projekt_Rent-A-Field/PregledSportskihProstoraFrm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Projekt_Rent_A_Field
+{
+    partial class PregledSportskihProstoraFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOdustani = new System.Windows.Forms.Button();
+            this.btnObrisi = new System.Windows.Forms.Button();
+            this.btnAzuriraj = new System.Windows.Forms.Button();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.dgvSportskiProstori = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSportskiProstori)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Sportski prostori:";
+            // 
+            // btnOdustani
+            // 
+            this.btnOdustani.Location = new System.Drawing.Point(249, 280);
+            this.btnOdustani.Name = "btnOdustani";
+            this.btnOdustani.Size = new System.Drawing.Size(194, 36);
+            this.btnOdustani.TabIndex = 10;
+            this.btnOdustani.Text = "Odustani";
+            this.btnOdustani.UseVisualStyleBackColor = true;
+            // 
+            // btnObrisi
+            // 
+            this.btnObrisi.Location = new System.Drawing.Point(249, 238);
+            this.btnObrisi.Name = "btnObrisi";
+            this.btnObrisi.Size = new System.Drawing.Size(194, 36);
+            this.btnObrisi.TabIndex = 9;
+            this.btnObrisi.Text = "Obrisi";
+            this.btnObrisi.UseVisualStyleBackColor = true;
+            // 
+            // btnAzuriraj
+            // 
+            this.btnAzuriraj.Location = new System.Drawing.Point(12, 280);
+            this.btnAzuriraj.Name = "btnAzuriraj";
+            this.btnAzuriraj.Size = new System.Drawing.Size(194, 36);
+            this.btnAzuriraj.TabIndex = 8;
+            this.btnAzuriraj.Text = "Azuriraj";
+            this.btnAzuriraj.UseVisualStyleBackColor = true;
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Location = new System.Drawing.Point(12, 238);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(194, 36);
+            this.btnDodaj.TabIndex = 7;
+            this.btnDodaj.Text = "Dodaj";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            // 
+            // dgvSportskiProstori
+            // 
+            this.dgvSportskiProstori.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSportskiProstori.Location = new System.Drawing.Point(12, 36);
+            this.dgvSportskiProstori.Name = "dgvSportskiProstori";
+            this.dgvSportskiProstori.Size = new System.Drawing.Size(431, 196);
+            this.dgvSportskiProstori.TabIndex = 6;
+            // 
+            // PregledSportskihProstoraFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(480, 370);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOdustani);
+            this.Controls.Add(this.btnObrisi);
+            this.Controls.Add(this.btnAzuriraj);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.dgvSportskiProstori);
+            this.Name = "PregledSportskihProstoraFrm";
+            this.Text = "PregledSportskihProstoraFrm";
+            this.Load += new System.EventHandler(this.PregledSportskihProstoraFrm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSportskiProstori)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOdustani;
+        private System.Windows.Forms.Button btnObrisi;
+        private System.Windows.Forms.Button btnAzuriraj;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.DataGridView dgvSportskiProstori;
+    }
+}
---
+++ Software/Projekt_Rent-A-Field/Projekt_Rent-A-Field/PregledSportskihProstoraFrm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt_Rent_A_Field
+{
+    public partial class PregledSportskihProstoraFrm : Form
+    {
+        public PregledSportskihProstoraFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void PregledSportskihProstoraFrm_Load(object sender, EventArgs e)
+        {
+            Osvjezi();
+        }
+
+        private void Osvjezi()
+        {
+            using (var context = new PI2306_DBEntities())
+            {
+                dgvSportskiProstori.DataSource = context.Sportski_prostor.ToList();
+            }
+            dgvSportskiProstori.Columns["sportski_prostor_id"].Visible = false;
+            dgvSportskiProstori.Columns["Rezervacija_prostora"].Visible = false;
+
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            DodavanjeSportskogProstoraFrm dodaj = new DodavanjeSportskogProstoraFrm();
+            dodaj.ShowDialog();
+
+            Osvjezi();
+        }
+
+        private void btnOdustani_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnObrisi_Click(object sender, EventArgs e)
+        {
+            Sportski_prostor odabrani = dgvSportskiProstori.CurrentRow.DataBoundItem as Sportski_prostor;
+
+            using (var context = new PI2306_DBEntities())
+            {
+                context.Sportski_prostor.Attach(odabrani);
+                context.Sportski_prostor.Remove(odabrani);
+                context.SaveChanges();
+            }
+            Osvjezi();
+        }
+
+        private void btnAzuriraj_Click(object sender, EventArgs e)
+        {
+            Sportski_prostor odabrani = dgvSportskiProstori.CurrentRow.DataBoundItem as Sportski_prostor;
+            AzuriranjeSportskihProstoraFrm from = new FrmAzuriranjeSportskihProstoraFrm(odabrani);
+            from.ShowDialog();
+            Osvjezi();
+        }
+    }
+}
