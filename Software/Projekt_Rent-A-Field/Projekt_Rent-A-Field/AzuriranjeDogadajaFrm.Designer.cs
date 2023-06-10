c827e2283cf931bd3b5eab23478b20ffb0f4ed6b
Petar Šemiga <psemiga@foi.hr>
2023-06-10 21:28:05 +0200

    Forma za azuriranje dogadaja

---
+++ Software/Projekt_Rent-A-Field/Projekt_Rent-A-Field/AzuriranjeDogadajaFrm.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Projekt_Rent_A_Field
+{
+    partial class AzuriranjeDogadajaFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtDuljina = new System.Windows.Forms.TextBox();
+            this.txtVrijemeOd = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePickerDatum = new System.Windows.Forms.DateTimePicker();
+            this.txtMAXbr = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOpis = new System.Windows.Forms.TextBox();
+            this.txtNaziv = new System.Windows.Forms.TextBox();
+            this.btnOdustani = new System.Windows.Forms.Button();
+            this.btnSpremi = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(315, 161);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(124, 13);
+            this.label7.TabIndex = 58;
+            this.label7.Text = "Duljina trajanja (broj sati):";
+            // 
+            // txtDuljina
+            // 
+            this.txtDuljina.Location = new System.Drawing.Point(314, 177);
+            this.txtDuljina.Name = "txtDuljina";
+            this.txtDuljina.Size = new System.Drawing.Size(269, 20);
+            this.txtDuljina.TabIndex = 57;
+            // 
+            // txtVrijemeOd
+            // 
+            this.txtVrijemeOd.Location = new System.Drawing.Point(314, 127);
+            this.txtVrijemeOd.Name = "txtVrijemeOd";
+            this.txtVrijemeOd.Size = new System.Drawing.Size(269, 20);
+            this.txtVrijemeOd.TabIndex = 56;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(311, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 13);
+            this.label6.TabIndex = 55;
+            this.label6.Text = "Sportski teren:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(314, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(269, 21);
+            this.comboBox1.TabIndex = 54;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(315, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 53;
+            this.label5.Text = "Vrijeme od:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(311, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 52;
+            this.label4.Text = "Datum:";
+            // 
+            // dateTimePickerDatum
+            // 
+            this.dateTimePickerDatum.Location = new System.Drawing.Point(313, 79);
+            this.dateTimePickerDatum.Name = "dateTimePickerDatum";
+            this.dateTimePickerDatum.Size = new System.Drawing.Size(270, 20);
+            this.dateTimePickerDatum.TabIndex = 51;
+            // 
+            // txtMAXbr
+            // 
+            this.txtMAXbr.Location = new System.Drawing.Point(11, 232);
+            this.txtMAXbr.Name = "txtMAXbr";
+            this.txtMAXbr.Size = new System.Drawing.Size(270, 20);
+            this.txtMAXbr.TabIndex = 50;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(8, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 13);
+            this.label3.TabIndex = 49;
+            this.label3.Text = "Maksimalan broj sudionika:";
+            // 
+            // txtOpis
+            // 
+            this.txtOpis.Location = new System.Drawing.Point(11, 79);
+            this.txtOpis.Multiline = true;
+            this.txtOpis.Name = "txtOpis";
+            this.txtOpis.Size = new System.Drawing.Size(270, 118);
+            this.txtOpis.TabIndex = 48;
+            // 
+            // txtNaziv
+            // 
+            this.txtNaziv.Location = new System.Drawing.Point(12, 31);
+            this.txtNaziv.Name = "txtNaziv";
+            this.txtNaziv.Size = new System.Drawing.Size(269, 20);
+            this.txtNaziv.TabIndex = 47;
+            // 
+            // btnOdustani
+            // 
+            this.btnOdustani.Location = new System.Drawing.Point(451, 218);
+            this.btnOdustani.Name = "btnOdustani";
+            this.btnOdustani.Size = new System.Drawing.Size(132, 34);
+            this.btnOdustani.TabIndex = 46;
+            this.btnOdustani.Text = "Odustani";
+            this.btnOdustani.UseVisualStyleBackColor = true;
+            // 
+            // btnSpremi
+            // 
+            this.btnSpremi.Location = new System.Drawing.Point(313, 218);
+            this.btnSpremi.Name = "btnSpremi";
+            this.btnSpremi.Size = new System.Drawing.Size(132, 34);
+            this.btnSpremi.TabIndex = 45;
+            this.btnSpremi.Text = "Azuriraj dogadaj";
+            this.btnSpremi.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(9, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 44;
+            this.label2.Text = "Opis:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 43;
+            this.label1.Text = "Naziv:";
+            // 
+            // AzuriranjeDogadajaFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(611, 282);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtDuljina);
+            this.Controls.Add(this.txtVrijemeOd);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePickerDatum);
+            this.Controls.Add(this.txtMAXbr);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtOpis);
+            this.Controls.Add(this.txtNaziv);
+            this.Controls.Add(this.btnOdustani);
+            this.Controls.Add(this.btnSpremi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AzuriranjeDogadajaFrm";
+            this.Text = "AzuriranjeDogadajaFrm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtDuljina;
+        private System.Windows.Forms.TextBox txtVrijemeOd;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDatum;
+        private System.Windows.Forms.TextBox txtMAXbr;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOpis;
+        private System.Windows.Forms.TextBox txtNaziv;
+        private System.Windows.Forms.Button btnOdustani;
+        private System.Windows.Forms.Button btnSpremi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Software/Projekt_Rent-A-Field/Projekt_Rent-A-Field/AzuriranjeDogadajaFrm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt_Rent_A_Field
+{
+    public partial class AzuriranjeDogadajaFrm : Form
+    {
+        private Dogadaj odabrani;
+        public AzuriranjeDogadajaFrm(Dogadaj dogadaj)
+        {
+            InitializeComponent();
+            odabrani = dogadaj;
+        }
+
+        private void btnSpremi_Click(object sender, EventArgs e)
+        {
+            using (var context = new PI2306_DBEntities())
+            {
+                string naziv = txtNaziv.Text;
+                string opis = txtOpis.Text;
+                int max = int.Parse(txtMAXbr.Text);
+
+                context.Dogadajs.Attach(odabrani);
+
+                odabrani.naziv = naziv;
+                odabrani.opis = opis;
+                odabrani.max_broj_sudionika = max;
+
+                foreach (var item in context.Rezervacija_prostora)
+                {
+                    if (odabrani.dogadaj_id == item.dogadaj_id)
+                    {
+                        DateTime datum = dateTimePickerDatum.Value.Date;
+                        string vrijeme = txtVrijemeOd.Text;
+                        int duljina = int.Parse(txtDuljina.Text);
+
+                        Sportski_prostor odabrani2 = comboBox1.SelectedItem as Sportski_prostor;
+
+                        context.Rezervacija_prostora.Attach(item);
+
+                        item.sportski_prostor_id = odabrani2.sportski_prostor_id;
+                        item.datum = datum;
+                        item.vrijeme_od = vrijeme;
+                        item.duljina_rezervacije = duljina;
+
+                    }
+                }
+
+                context.SaveChanges();
+            }
+            Close();
+        }
+
+        private void FrmAzuriranjeDogadaja_Load(object sender, EventArgs e)
+        {
+            txtNaziv.Text = odabrani.naziv;
+            txtOpis.Text = odabrani.opis;
+            txtMAXbr.Text = odabrani.max_broj_sudionika.ToString();
+            PopuniSportskeProstore();
+            using (var context = new PI2306_DBEntities())
+            {
+                context.Dogadajs.Attach(odabrani);
+                foreach (var item in context.Rezervacija_prostora)
+                {
+                    if (odabrani.dogadaj_id == item.dogadaj_id)
+                    {
+                        context.Rezervacija_prostora.Attach(item);
+                        dateTimePickerDatum.Value = item.datum.Value;
+                        txtVrijemeOd.Text = item.vrijeme_od;
+                        txtDuljina.Text = item.duljina_rezervacije.ToString();
+                        foreach (var item2 in context.Sportski_prostor)
+                        {
+                            if (item.sportski_prostor_id == item2.sportski_prostor_id)
+                            {
+                                comboBox1.Text = item2.naziv;
+                            }
+
+                        }
+                    }
+
+                }
+                context.SaveChanges();
+            }
+
+        }
+        private object PopuniSportskeProstore()
+        {
+            using (var context = new PI2306_DBEntities())
+            {
+                comboBox1.DataSource = context.Sportski_prostor.ToList();
+            }
+            return comboBox1.DataSource;
+        }
+
+        private void btnOdustani_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
