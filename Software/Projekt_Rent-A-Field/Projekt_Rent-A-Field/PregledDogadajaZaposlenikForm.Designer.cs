31e15efe6af895df92317bb58c727a740286347d
Petar Šemiga <psemiga@foi.hr>
2023-06-10 21:22:48 +0200

    Forma za pregled dogadaja kod zaposlenika

---
+++ Software/Projekt_Rent-A-Field/Projekt_Rent-A-Field/PregledDogadajaZaposlenikForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Projekt_Rent_A_Field
+{
+    partial class PregledDogadajaZaposlenikForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAzuriraj = new System.Windows.Forms.Button();
+            this.btnIzbrisi = new System.Windows.Forms.Button();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.btnPoProstoru = new System.Windows.Forms.Button();
+            this.cmbProstor = new System.Windows.Forms.ComboBox();
+            this.btnDatum = new System.Windows.Forms.Button();
+            this.btnPrikaziSve = new System.Windows.Forms.Button();
+            this.dgvRezervacija = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRezervacija)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAzuriraj
+            // 
+            this.btnAzuriraj.Location = new System.Drawing.Point(93, 302);
+            this.btnAzuriraj.Name = "btnAzuriraj";
+            this.btnAzuriraj.Size = new System.Drawing.Size(75, 48);
+            this.btnAzuriraj.TabIndex = 27;
+            this.btnAzuriraj.Text = "Azuriraj";
+            this.btnAzuriraj.UseVisualStyleBackColor = true;
+            // 
+            // btnIzbrisi
+            // 
+            this.btnIzbrisi.Location = new System.Drawing.Point(174, 302);
+            this.btnIzbrisi.Name = "btnIzbrisi";
+            this.btnIzbrisi.Size = new System.Drawing.Size(75, 48);
+            this.btnIzbrisi.TabIndex = 26;
+            this.btnIzbrisi.Text = "Izbrisi";
+            this.btnIzbrisi.UseVisualStyleBackColor = true;
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Location = new System.Drawing.Point(12, 302);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(75, 48);
+            this.btnDodaj.TabIndex = 25;
+            this.btnDodaj.Text = "Dodaj";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            // 
+            // btnPoProstoru
+            // 
+            this.btnPoProstoru.Location = new System.Drawing.Point(604, 19);
+            this.btnPoProstoru.Name = "btnPoProstoru";
+            this.btnPoProstoru.Size = new System.Drawing.Size(132, 20);
+            this.btnPoProstoru.TabIndex = 24;
+            this.btnPoProstoru.Text = "Prikazi po prostoru";
+            this.btnPoProstoru.UseVisualStyleBackColor = true;
+            // 
+            // cmbProstor
+            // 
+            this.cmbProstor.FormattingEnabled = true;
+            this.cmbProstor.Location = new System.Drawing.Point(398, 18);
+            this.cmbProstor.Name = "cmbProstor";
+            this.cmbProstor.Size = new System.Drawing.Size(198, 21);
+            this.cmbProstor.TabIndex = 23;
+            // 
+            // btnDatum
+            // 
+            this.btnDatum.Location = new System.Drawing.Point(165, 19);
+            this.btnDatum.Name = "btnDatum";
+            this.btnDatum.Size = new System.Drawing.Size(132, 20);
+            this.btnDatum.TabIndex = 22;
+            this.btnDatum.Text = "Prikazi po datumu";
+            this.btnDatum.UseVisualStyleBackColor = true;
+            // 
+            // btnPrikaziSve
+            // 
+            this.btnPrikaziSve.Location = new System.Drawing.Point(12, 272);
+            this.btnPrikaziSve.Name = "btnPrikaziSve";
+            this.btnPrikaziSve.Size = new System.Drawing.Size(132, 20);
+            this.btnPrikaziSve.TabIndex = 21;
+            this.btnPrikaziSve.Text = "Prikazi sve";
+            this.btnPrikaziSve.UseVisualStyleBackColor = true;
+            // 
+            // dgvRezervacija
+            // 
+            this.dgvRezervacija.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRezervacija.Location = new System.Drawing.Point(12, 45);
+            this.dgvRezervacija.Name = "dgvRezervacija";
+            this.dgvRezervacija.Size = new System.Drawing.Size(724, 221);
+            this.dgvRezervacija.TabIndex = 20;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 19);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(147, 20);
+            this.dateTimePicker1.TabIndex = 19;
+            // 
+            // PregledDogadajaZaposlenikForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(800, 370);
+            this.Controls.Add(this.btnAzuriraj);
+            this.Controls.Add(this.btnIzbrisi);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.btnPoProstoru);
+            this.Controls.Add(this.cmbProstor);
+            this.Controls.Add(this.btnDatum);
+            this.Controls.Add(this.btnPrikaziSve);
+            this.Controls.Add(this.dgvRezervacija);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "PregledDogadajaZaposlenikForm";
+            this.Text = "PregledDogadajaZaposlenikForm";
+            this.Load += new System.EventHandler(this.PregledDogadajaZaposlenikForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRezervacija)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAzuriraj;
+        private System.Windows.Forms.Button btnIzbrisi;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.Button btnPoProstoru;
+        private System.Windows.Forms.ComboBox cmbProstor;
+        private System.Windows.Forms.Button btnDatum;
+        private System.Windows.Forms.Button btnPrikaziSve;
+        private System.Windows.Forms.DataGridView dgvRezervacija;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
---
+++ Software/Projekt_Rent-A-Field/Projekt_Rent-A-Field/PregledDogadajaZaposlenikForm.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt_Rent_A_Field
+{
+    public partial class PregledDogadajaZaposlenikForm : Form
+    {
+        public PregledDogadajaZaposlenikForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PregledDogadajaZaposlenikForm_Load(object sender, EventArgs e)
+        {
+            PopuniSportskeProstore();
+        }
+
+        private void PrikaziSve()
+        {
+            using (var context = new PI2306_DBEntities())
+            {
+
+                var query = from d in context.Dogadajs
+                            select d;
+
+                dgvRezervacija.DataSource = query.ToList();
+
+            }
+            dgvRezervacija.Columns["Rezervacija_prostora"].Visible = false;
+        }
+
+        private void PrikaziPoDatumu()
+        {
+            DateTime odabranidatum = dateTimePicker1.Value.Date;
+            using (var context = new PI2306_DBEntities())
+            {
+                if (odabranidatum != null)
+                {
+                    var query = from rp in context.Rezervacija_prostora
+                                from d in context.Dogadajs
+                                from sp in context.Sportski_prostor
+                                where rp.dogadaj_id == d.dogadaj_id
+                                where rp.sportski_prostor_id == sp.sportski_prostor_id
+                                where rp.datum == odabranidatum
+                                select d;
+
+                    dgvRezervacija.DataSource = query.ToList();
+                }
+
+                else
+                {
+                    dgvRezervacija.DataSource = null;
+                }
+                dgvRezervacija.Columns["Rezervacija_prostora"].Visible = false;
+
+            }
+        }
+
+        private void PrikaziPoProstoru()
+        {
+            Sportski_prostor odabrani = cmbProstor.SelectedItem as Sportski_prostor;
+            using (var context = new PI2306_DBEntities())
+            {
+                if (odabrani != null)
+                {
+                    var query = from rp in context.Rezervacija_prostora
+                                from d in context.Dogadajs
+                                from sp in context.Sportski_prostor
+                                where rp.dogadaj_id == d.dogadaj_id
+                                where rp.sportski_prostor_id == sp.sportski_prostor_id
+                                where sp.naziv == odabrani.naziv
+                                select d;
+
+                    dgvRezervacija.DataSource = query.ToList();
+                }
+                else
+                {
+                    dgvRezervacija.DataSource = null;
+                }
+                dgvRezervacija.Columns["Rezervacija_prostora"].Visible = false;
+
+            }
+        }
+
+        private object PopuniSportskeProstore()
+        {
+            using (var context = new PI2306_DBEntities())
+            {
+                cmbProstor.DataSource = context.Sportski_prostor.ToList();
+            }
+            return cmbProstor.DataSource;
+        }
+
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            KreiranjeDogadajaFrm form = new KreiranjeDogadajaFrm();
+            form.ShowDialog();
+            PrikaziSve();
+        }
+
+        private void btnDatum_Click(object sender, EventArgs e)
+        {
+            PrikaziPoDatumu();
+        }
+
+        private void btnPoProstoru_Click(object sender, EventArgs e)
+        {
+            PrikaziPoProstoru();
+        }
+
+        private void btnPrikaziSve_Click(object sender, EventArgs e)
+        {
+            PrikaziSve();
+        }
+
+        private void btnIzbrisi_Click(object sender, EventArgs e)
+        {
+            Dogadaj odabrani = dgvRezervacija.CurrentRow.DataBoundItem as Dogadaj;
+
+            using (var context = new PI2306_DBEntities())
+            {
+                context.Dogadajs.Attach(odabrani);
+                context.Dogadajs.Remove(odabrani);
+                foreach (var item in context.Rezervacija_prostora)
+                {
+                    if (odabrani.dogadaj_id == item.dogadaj_id)
+                    {
+                        context.Rezervacija_prostora.Attach(item);
+                        context.Rezervacija_prostora.Remove(item);
+                    }
+                }
+                context.SaveChanges();
+            }
+            PrikaziSve();
+        }
+
+        private void btnAzuriraj_Click(object sender, EventArgs e)
+        {
+            Dogadaj odabrani = dgvRezervacija.CurrentRow.DataBoundItem as Dogadaj;
+            AzuriranjeDogadajaFrm frm = new AzuriranjeDogadajaFrm(odabrani);
+            frm.ShowDialog();
+            PrikaziSve();
+        }
+    }
+}
