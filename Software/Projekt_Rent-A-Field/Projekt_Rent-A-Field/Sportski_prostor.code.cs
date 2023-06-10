95114f9a9de787a56052380bf723f26e0aa20f84
Petar Šemiga <psemiga@foi.hr>
2023-06-10 17:57:35 +0200

    Vracanje naziva za Sportski prostor

---
+++ Software/Projekt_Rent-A-Field/Projekt_Rent-A-Field/Sportski_prostor.code.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Rent_A_Field
+{
+    public partial class Sportski_prostor
+    {
+        public override string ToString()
+        {
+            return naziv;
+        }
+    }
+}
