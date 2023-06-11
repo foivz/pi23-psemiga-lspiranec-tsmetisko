cf54c856102022c8fd6e137bd0c7d7edc7023834
David Milanović <dmilanovi@foi.hr>
2023-06-11 20:29:48 +0200

    Ažurirana forma DodajProstorFrm

--- Software/Projekt_Rent-A-Field/Projekt_Rent-A-Field/DodajProstorFrm.cs
+++ Software/Projekt_Rent-A-Field/Projekt_Rent-A-Field/DodajProstorFrm.cs
@@ -19,22 +19,23 @@ namespace Projekt_Rent_A_Field
 
         private void DodajProstorFrm_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void ButtonDodajProstor_Click(object sender, EventArgs e)
+        {
             using (var context = new PI2306_DBEntities())
             {
                 Sportski_prostor novi = new Sportski_prostor();
                 novi.naziv = textBoxNazivProstor.Text;
                 novi.adresa = textBoxAdresaProstor.Text;
-                novi.cijena = double.Parse(textBoxCijenaPoSatuProstor.Text);
+                novi.cijena = float.Parse(textBoxCijenaPoSatuProstor.Text);
 
                 context.Sportski_prostor.Add(novi);
                 context.SaveChanges();
 
             }
             Close();
-        }
-
-        private void ButtonDodajProstor_Click(object sender, EventArgs e)
-        {
 
         }
 
